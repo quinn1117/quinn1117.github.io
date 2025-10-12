bf025c92dd3f399af5e55a370c91e2d66fa239a6
quinn111111 <77837955+quinn111111@users.noreply.github.com>
2025-10-12 15:52:43 +0800

    Site updated: 2025-10-12 15:52:43

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1760255307733";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1760255559050";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
